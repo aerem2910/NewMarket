c66aec17cd69f4153ef8c8767824bb54e2253242
aerem2910 <138059430+aerem2910@users.noreply.github.com>
2024-02-06 22:54:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,63 +1,53 @@
-
-using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using StoreMarket.Abstractions;
 using StoreMarket.Contexts;
 using StoreMarket.Mappers;
 using StoreMarket.Services;
 
-namespace StoreMarket
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddDbContext<StoreContext>();
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-
-            builder.Host.ConfigureContainer<ContainerBuilder>(c =>
-                                            c.RegisterType<ProductServices>()
-                                            .As<IProductServices>());
-
-            builder.Services.AddMemoryCache(m=>m.TrackStatistics = true);
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            var configuration = new ConfigurationBuilder()
+        var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
             .Build();
 
-            builder.Services.AddDbContext<StoreContext>(options =>
-            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StoreDB;TrustServerCertificate=True;");
+        builder.Services.AddDbContext<StoreContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(typeof(MappingProfile));
+        builder.Services.AddMemoryCache(m => m.TrackStatistics = true);
+        builder.Services.AddScoped<IProductServices, ProductServices>();
 
-            app.UseAuthorization();
+        builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
+        //builder.Services.AddMemoryCache(m=>m.TrackStatistics = true); // удалите эту строку
 
-            app.MapControllers();
+        // Перенесите вызов AddDbContext перед созданием WebApplication
+        builder.Services.AddDbContext<StoreContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("Server=(localdb)\\mssqllocaldb;Database=StoreDB;TrustServerCertificate=True;")));
 
-            app.Run();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
+
